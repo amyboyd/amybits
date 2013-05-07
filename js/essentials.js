ad68998411a59dceccb2de4c6449fe4bdd759aff
Amy Boyd <amy@amyboyd.co.uk>
2013-05-08 00:39:45 +0100

    Code cosmetics.

--- js/essentials.js
+++ js/essentials.js
@@ -1,13 +1,6 @@
 // IE doesn't style elements it doesn't recognise.
 // This must be run before anything else.
 // See http://net.tutsplus.com/tutorials/html-css-techniques/how-to-make-all-browsers-render-html5-mark-up-correctly-even-ie6/
-document.createElement('article');
-document.createElement('aside');
-document.createElement('figcaption');
-document.createElement('figure');
-document.createElement('footer');
-document.createElement('header');
-document.createElement('hgroup');
-document.createElement('nav');
-document.createElement('section');
-document.createElement('summary');
+'article,aside,figcaption,figure,footer,header,hgroup,nav,section,summary'.split(',').map(function(it) {
+  document.createElement(it);
+});
