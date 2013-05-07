73c0dc63ad32807fef324e319c2a2450af7f4acd
Amy Boyd <amy@amyboyd.co.uk>
2013-05-07 23:50:00 +0100

    Create HTML5 elements so IE 8 and below will properly style them.

---
+++ js/essentials.js
@@ -0,0 +1,13 @@
+// IE doesn't style elements it doesn't recognise.
+// This must be run before anything else.
+// See http://net.tutsplus.com/tutorials/html-css-techniques/how-to-make-all-browsers-render-html5-mark-up-correctly-even-ie6/
+document.createElement('article');
+document.createElement('aside');
+document.createElement('figcaption');
+document.createElement('figure');
+document.createElement('footer');
+document.createElement('header');
+document.createElement('hgroup');
+document.createElement('nav');
+document.createElement('section');
+document.createElement('summary');
